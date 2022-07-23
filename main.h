ac42de20f427384aaa85e03bb4ed731e3cacb25e
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-07-23 16:32:23 -0500

    Update main.h
    Signed-off-by: David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>

--- main.h
+++ main.h
@@ -21,9 +21,9 @@ int _putchar(char c);
 int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
-int print_d(va_list d)
+int print_d(va_list d);
 
-/* Advanced Functions  */
+/* Advanced Functions */
 int print_b(va_list b);
 int print_u(va_list u);
 int print_o(va_list o);
