d52336a2643ce1423716bd8c841405f6a9a980fe
LiJoBaZar <lijobazar@gmail.com>
2022-07-23 17:45:39 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,64 +1,13 @@
-<<<<<<< HEAD
-#include "main.h"                                                                                                                               
-
-/*_printf - Output to format.
- *@format: Output a character string.
- *Return: The number of characters printed; excluding the null byte.
- */
-
-int _printf(const char *format, ...)
-
-{
-	int i = 0, j = 0, a = 0;
-	va_list ap;
-	
-		if (format == NULL || (strlen(format) == 1 && format[0] == '%'))
-		{
-			return (-1);
-		}
-		va_start(ap, format);
-		while (format && format[i])
-		{
-			if (format[i] != '%')
-			{
-				putchar(format[i]);
-				j++;
-			}
-			if (format[i] == '%' && format[i + 1] != 'K' && format[i + 1] != '!')
-			{
-				a = g_printf(*(format + (i + 1)), ap);
-				if(a != 0){
-					j = j + a;
-					i = i + 2;
-				}
-				continue;
-				if (*(format + (i + 1)) == '\0')
-				{
-					putchar(format[i]);
-					j++;
-				}
-			}
-			else if ((format[i] == '%' && format[i + 1] == 'K') ||
-			(format[i] == '%' && format[i + 1] == '!'))
-			{
-				putchar(format[i]);
-				j++;
-			}
-			i++;
-	}
-	va_end(ap);
-	return (j);
-=======
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
 
 /**
- * check_format - checks if there is a valid format specifier
- * @format: possible valid format specifier
- * Return: pointer to valid function or NULL
+ *check_format - checks if there is a valid format specifier
+ *@format: possible valid format specifier
+ *Return: pointer to valid function or NULL
  */
- 
+
 int (*check_format(const char *format))(va_list)
 {
 	int i = 0;
@@ -88,20 +37,20 @@ int (*check_format(const char *format))(va_list)
 }
 
 /**
- * _printf - function for format printing
- * @format: list of arguments to printing
- * Return: Number of characters to printing
+ *_printf - function for format printing
+ *@format: list of arguments to printing
+ *Return: Number of characters to printing
  */
- 
+
 int _printf(const char *format, ...)
 {
 	va_list name;
 	int (*function)(va_list);
 	unsigned int i = 0, counter = 0;
-	
+
 	if (format == NULL)
 		return (-1);
-		
+
 	va_start(name, format);
 	while (format && format[i])
 	{
@@ -134,5 +83,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(name);
 	return (counter);
->>>>>>> 64bf14db5937c78e198a75250823e92c22d638ff
 }
